ebf0579999d183557891e907654350557f2cef46
sarvendra <sarvendraanand@gmail.com>
2016-05-30 12:22:08 +0530

    Add basic color thief code

--- src/main/java/com/learning/Application.java
+++ src/main/java/com/learning/Application.java
@@ -1,10 +1,21 @@
 package com.learning;
 
+import java.io.File;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+	public static String ROOT = "upload-dir";
+    @Bean
+    CommandLineRunner init() {
+        return (String[] args) -> {
+            new File(ROOT).mkdir();
+        };
+    }
     public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/com/learning/controller/MainController.java
+++ src/main/java/com/learning/controller/MainController.java
@@ -1,10 +1,29 @@
 package com.learning.controller;
 
+import com.learning.Application;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class MainController {
@@ -14,5 +33,55 @@ public class MainController {
         response.setContentType("text/plain");
         response.setCharacterEncoding("UTF-8");
         return "alive";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String provideUploadInfo(Model model) {
+//        File rootFolder = new File(Application.ROOT);
+//        List<String> fileNames = Arrays.stream(rootFolder.listFiles())
+//                .map(f -> f.getName())
+//                .collect(Collectors.toList());
+//
+//        model.addAttribute("files",
+//                Arrays.stream(rootFolder.listFiles())
+//                        .sorted(Comparator.comparingLong(f -> -1 * f.lastModified()))
+//                        .map(f -> f.getName())
+//                        .collect(Collectors.toList())
+//        );
+        return "uploadForm";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    public String handleFileUpload(@RequestParam("name") String name,
+                                   @RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+//        if (name.contains("/")) {
+//            redirectAttributes.addFlashAttribute("message", "Folder separators not allowed");
+//            return "redirect:/";
+//        }
+//        if (name.contains("/")) {
+//            redirectAttributes.addFlashAttribute("message", "Relative pathnames not allowed");
+//            return "redirect:/";
+//        }
+
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(new File(Application.ROOT + "/" + name)));
+                FileCopyUtils.copy(file.getInputStream(), stream);
+                stream.close();
+                redirectAttributes.addFlashAttribute("message",
+                        "You successfully uploaded " + name + "!");
+            }
+            catch (Exception e) {
+                redirectAttributes.addFlashAttribute("message",
+                        "You failed to upload " + name + " => " + e.getMessage());
+            }
+        }
+        else {
+            redirectAttributes.addFlashAttribute("message",
+                    "You failed to upload " + name + " because the file was empty");
+        }
+        return "redirect:/";
     }
 }
